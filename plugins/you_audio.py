098350e8e9f2bc4e3be1939f2d13a1495d42f2f4
Ningmua Bruno <gwatanajohn3@gmail.com>
2018-12-13 11:22:12 +0100

    Update you_audio.py

--- plugins/you_audio.py
+++ plugins/you_audio.py
@@ -22,17 +22,17 @@ def search_query_yt(query):
 	list_videos = []
 	for link in soup.find_all('a'):
 		url = link.get('href')
-		titles = link.get('title')
-		if url.startswith("/watch") and (id_url!=url) and (titles!=None):
+		title = link.get('title')
+		if url.startswith("/watch") and (id_url!=url) and (title!=None):
 			id_url = url
-			dic = {'title':titles,'url':url_yt+url}
+			dic = {'title':title,'url':url_yt+url}
 			list_videos.append(dic)
 		else:
 			pass
 	dic = {'bot_api_yt':list_videos}
 	return dic
 
-def downloads(message, client, sent_id, text, msg_id,nome):
+def ydownload(message, client, sent_id, text, msg_id,nome):
 	t1 = time.time()
 	res = subprocess.getstatusoutput("""youtube-dl {}""".format(text))[1]
 	re = []
@@ -40,21 +40,21 @@ def downloads(message, client, sent_id, text, msg_id,nome):
 		re.append(i)
 	for i in re:
 		if '[download] ' in i:
-			if '.m4a' in i:
-				titles = i
-	titles = titles.replace('[download] ','')
-	if ' has already been downloaded' in titles:
-		titles = titles.replace(' has already been downloaded','')
-	if 'Destination: ' in titles:
-		titles = titles.replace('Destination: ','')
-	print(titles)
+			if '.mp4' in i or '.webm' in i:
+				title = i
+	title = title.replace('[download] ','')
+	if ' has already been downloaded' in title:
+		title = title.replace(' has already been downloaded','')
+	if 'Destination: ' in title:
+		title = title.replace('Destination: ','')
+	print(title)
 	a = re[0]
 	print(a)
 	number = '-'+a.replace('[youtube] ','').replace(': Downloading webpage','')
 	client.edit_message_caption(message.chat.id, sent_id,'Sending {}'.format(nome))
 	try:
 		client.send_chat_action(message.chat.id,'UPLOAD_AUDIO')
-		sent = client.send_document(message.chat.id,titles,caption=nome,reply_to_message_id=msg_id).message_id
+		sent = client.send_document(message.chat.id,title,caption=nome,reply_to_message_id=msg_id).message_id
 		t2 = time.time()
 		client.edit_message_caption(message.chat.id,sent,caption='{}\nCompleted in {} Seconds'.format(nome,str(int(t2-t1))))
 		client.delete_messages(message.chat.id, sent_id)
@@ -71,7 +71,7 @@ def yaudio(message,client):
 	if text == '':
 		client.send_message(
 			chat_id=chat_id,
-			text='Uso: /ymp3 URL do vídeo or name',
+			text='Uso: /ytdl URL do vídeo ou nome',
 			reply_to_message_id=msg_id
 		)
 	elif 'youtu.be' in text or 'youtube.com' in text:
@@ -82,7 +82,7 @@ def yaudio(message,client):
 			reply_to_message_id=msg_id
 		).message_id
 		a = search_query_yt(text)
-		titles = a['bot_api_yt'][0]['title']
+		title = a['bot_api_yt'][0]['title']
 		thumb = a['bot_api_yt'][0]['url'].split('v=')[1]
 	else:
 		sent_id = client.send_message(
@@ -93,13 +93,13 @@ def yaudio(message,client):
 		).message_id
 		a = search_query_yt(text)
 		text = a['bot_api_yt'][0]['url']
-		titles = a['bot_api_yt'][0]['title']
+		title = a['bot_api_yt'][0]['title']
 		thumb = text.split('v=')[1]
 	client.delete_messages(message.chat.id, sent_id)
 	print('https://i.ytimg.com/vi/{}/hqdefault.jpg'.format(thumb))
 	try:
-		sent_id = client.send_photo(message.chat.id,'https://i.ytimg.com/vi/{}/hqdefault.jpg'.format(thumb) ,caption='Downloading: {}'.format(titles)).message_id
+		sent_id = client.send_photo(message.chat.id,'https://i.ytimg.com/vi/{}/hqdefault.jpg'.format(thumb) ,caption='Downloading: {}'.format(title)).message_id
 	except:
-		sent_id = client.send_photo(message.chat.id,'yt.png' ,caption='Downloading: {}'.format(titles)).message_id
-	nome = titles
-	exec_thread(downloads,message,client,sent_id,text,msg_id,nome)
+		sent_id = client.send_photo(message.chat.id,'yt.png' ,caption='Downloading: {}'.format(title)).message_id
+	nome = title
+	exec_thread(ydownload,message,client,sent_id,text,msg_id,nome)
