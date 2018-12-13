95d38a41495ebca5759de4927f6a8c87cef618f1
Ningmua Bruno <gwatanajohn3@gmail.com>
2018-12-14 00:16:21 +0100

    Update you_audio.py

--- plugins/you_audio.py
+++ plugins/you_audio.py
@@ -24,8 +24,6 @@ def DownLoadFile(url, file_name):
 
     # create download directory, if not exist
 
-if not os.path.isdir(Config.DOWNLOAD_LOCATION):
-	os.makedirs(Config.DOWNLOAD_LOCATION)
 from translation import Translation
 def humanbytes(size):
     # https://stackoverflow.com/a/49361727/4723940
@@ -62,9 +60,11 @@ def search_query_yt(query):
 	return dic
 
 def dld(message, client, sent_id, text, msg_id,nome):
+	if not os.path.isdir(Config.DOWNLOAD_LOCATION):
+		os.makedirs(Config.DOWNLOAD_LOCATION)
 	t1 = time.time()
 	dldir = Config.DOWNLOAD_LOCATION + "/" + text 
-	FORMAT_SELECTION = "Select the desired format: <a href='{}'>file size might be approximate</a>"
+	FORMAT_SELECTION = "Downloading the song in mp3 format <a href='{}'>With the best Quality</a>"
 	command_to_exec = ["youtube-dl", "--no-warnings", "-j", text]
 	t_response = subprocess.check_output(command_to_exec, stderr=subprocess.STDOUT)
 	x_reponse = t_response.decode("UTF-8")
@@ -94,147 +94,56 @@ def dld(message, client, sent_id, text, msg_id,nome):
 		response_json["thumbnail"]
 	
 	thumb_image_path = DownLoadFile(thumbnail_image, Config.DOWNLOAD_LOCATION + "/" + ytitle + ".jpg")
-	client.send_message(
-                    message.chat.id,
-                    text=FORMAT_SELECTION.format(thumbnail),
-                    reply_markup=reply_markup,
-                    parse_mode=pyrogram.ParseMode.HTML,
-                    reply_to_message_id=msg_id
-                )
-	
-def button(bot, update):
-    if update.data.find(":") == -1:
-        return ""
-    youtube_dl_format, youtube_dl_ext = update.data.split(":")
-    youtube_dl_url = update.message.reply_to_message.text
-    thumb_image_path = Config.DOWNLOAD_LOCATION + "/" + str(update.from_user.id) + ".jpg"
-    bot.edit_message_text(
-        text=Translation.DOWNLOAD_START,
-        chat_id=update.from_user.id,
-        message_id=update.message.message_id
-    )
-    description = " " + " \r\n© @AnyDLBot"
-    download_directory = ""
-    command_to_exec = []
-    if "mp3" in youtube_dl_ext:
-        download_directory = Config.DOWNLOAD_LOCATION + "/" + str(update.from_user.id) + "_" + youtube_dl_format + "." + youtube_dl_ext + ""
-        command_to_exec = [
+	client.edit_message_caption(message.chat.id, sent_id,FORMAT_SELECTION.format(thumbnail))
+	try:
+		youtube_dl_format = "0"
+		youtube_dl_ext = "mp3"
+    		youtube_dl_url = text
+    		thumb_image_path = Config.DOWNLOAD_LOCATION + "/" + ytitle + ".jpg"
+		sentid = client.send_message(
+			chat_id=chat_id,
+			text=Translation.DOWNLOAD_START,
+			parse_mode='Markdown',
+			reply_to_message_id=msg_id
+		).message_id
+		message.delete(sentid)
+		description = " " + " \r\n© @Bfas237Bots"
+    		download_directory = ""
+		download_directory = Config.DOWNLOAD_LOCATION + "/" + ytitle + "_" + youtube_dl_format + "." + youtube_dl_ext + ""
+		command_to_exec = [
             "youtube-dl",
             "--extract-audio",
             "--audio-format", youtube_dl_ext,
             "--audio-quality", youtube_dl_format,
             youtube_dl_url,
-            "-o", download_directory
-        ]
-    else:
-        download_directory = Config.DOWNLOAD_LOCATION + "/" + str(update.from_user.id) + "_" + youtube_dl_format + "." + youtube_dl_ext + ".mp4"
-        # command_to_exec = ["youtube-dl", "-f", youtube_dl_format, "--hls-prefer-ffmpeg", "--recode-video", "mp4", "-k", youtube_dl_url, "-o", download_directory]
-        command_to_exec = [
-            "youtube-dl",
-            "--embed-subs",
-            "-f", youtube_dl_format,
-            "--recode-video", "mp4", "-k",
-            "--hls-prefer-ffmpeg", youtube_dl_url,
-            "-o", download_directory
-        ]
-    logger.info(command_to_exec)
-    try:
-        t_response = subprocess.check_output(command_to_exec, stderr=subprocess.STDOUT)
-    except subprocess.CalledProcessError as exc:
-        # print("Status : FAIL", exc.returncode, exc.output)
-        bot.edit_message_text(
-            chat_id=update.from_user.id,
-            message_id=update.message.message_id,
-            text=exc.output.decode("UTF-8"),
-            # reply_markup=reply_markup
-        )
-    else:
-        logger.info(t_response)
-        bot.edit_message_text(
-            text=Translation.UPLOAD_START,
-            chat_id=update.from_user.id,
-            message_id=update.message.message_id
-        )
-        file_size = os.stat(download_directory).st_size
-        if file_size > Config.TG_MAX_FILE_SIZE:
-            bot.edit_message_text(
-                text=Translation.RCHD_TG_API_LIMIT,
-                chat_id=update.from_user.id,
-                message_id=update.message.message_id
-            )
-        else:
-            # try to upload file
-            if download_directory.endswith("mp3"):
-                bot.send_audio(
-                    chat_id=update.from_user.id,
-                    audio=download_directory,
-                    caption=description,
-                    # duration=response_json["duration"],
-                    # performer=response_json["uploader"],
-                    # title=response_json["title"],
-                    # reply_markup=reply_markup,
-                    thumb=thumb_image_path,
-                    reply_to_message_id=update.message.reply_to_message.message_id
-                )
-            elif download_directory.endswith("mp4"):
-                bot.send_video(
-                    chat_id=update.from_user.id,
-                    video=download_directory,
-                    caption=description,
-                    # duration=response_json["duration"],
-                    # width=response_json["width"],
-                    # height=response_json["height"],
-                    supports_streaming=True,
-                    # reply_markup=reply_markup,
-                    thumb=thumb_image_path,
-                    reply_to_message_id=update.message.reply_to_message.message_id
-                )
-            else:
-                bot.send_document(
-                    chat_id=update.from_user.id,
-                    document=download_directory,
-                    caption=description,
-                    # reply_markup=reply_markup,
-                    thumb=thumb_image_path,
-                    reply_to_message_id=update.message.reply_to_message.message_id
-                )
-            os.remove(download_directory)
-            os.remove(thumb_image_path)
-            bot.edit_message_text(
-                text=Translation.AFTER_SUCCESSFUL_UPLOAD_MSG,
-                chat_id=update.from_user.id,
-                message_id=update.message.message_id,
-                disable_web_page_preview=True
-            )
-
-	
-	
-	
-	
-	
-	
-	
-	try:
+            "-o", download_directory]
+		if ' - ' in ytitle:
+			performer, title = ytitle.rsplit(' - ',1)
+ 		else:
+                        performer = None
+			title = ytitle
 		
-		client.send_chat_action(message.chat.id,'UPLOAD_VIDEO')
-		sent = client.send_document(message.chat.id,title,caption=nome,reply_to_message_id=msg_id).message_id
+		client.send_chat_action(message.chat.id,'UPLOAD_AUDIO')
+		sent = client.send_audio(message.chat.id, audio=download_directory, caption=description, performer=performer, title=title, thumb=thumb_image_path, reply_to_message_id=msg_id).message_id
 		t2 = time.time()
-		client.edit_message_caption(message.chat.id,sent,caption='{}\nCompleted in {} Seconds'.format(nome,str(int(t2-t1))))
+		client.edit_message_caption(message.chat.id,sent,caption='{}\nCompleted in {} Seconds'.format(description,str(int(t2-t1))))
 		client.delete_messages(message.chat.id, sent_id)
 	except Exception as error:
-		client.edit_message_caption(message.chat.id, sent_id,'Could not send the video')
+		client.edit_message_caption(message.chat.id, sent_id,'Could not send the mp3 file')
 		print(error)
 	client.send_chat_action(message.chat.id,'CANCEL')
 	os.remove(title)
+	
+	
 
 def audio(message,client):
-	text = message.text[6:]
+	text = message.text[5:]
 	chat_id = message.chat.id
 	msg_id = message.message_id
 	if text == '':
 		client.send_message(
 			chat_id=chat_id,
-			text='Uso: /ytdl URL do vídeo ou nome',
+			text='Use: /mp3 video link or Video name',
 			reply_to_message_id=msg_id
 		)
 	elif 'youtu.be' in text or 'youtube.com' in text:
@@ -266,9 +175,7 @@ def audio(message,client):
 		sent_id = client.send_photo(message.chat.id,'yt.png' ,caption='Downloading: {}'.format(title)).message_id
 	nome = title
 	exec_thread(dld,message,client,sent_id,text,msg_id,nome)
-app = config.app
-app.add_handler(pyrogram.CallbackQueryHandler(button))
-	
+
 	
 	
 	
