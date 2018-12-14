69e81a4caf7a76ea99a1ad8e495892f9cdae3a8d
Ningmua Bruno <gwatanajohn3@gmail.com>
2018-12-14 01:37:24 +0100

    Update you_audio.py

--- plugins/you_audio.py
+++ plugins/you_audio.py
@@ -98,13 +98,15 @@ def dld(message, client, sent_id, text, msg_id,nome):
         download_directory = " "
         download_directory = Config.DOWNLOAD_LOCATION + "/" + ytitle + "_" + youtube_dl_format + "." + youtube_dl_ext + ""
         command_to_exec = ["youtube-dl", "--extract-audio", "--audio-format", youtube_dl_ext,"--audio-quality", youtube_dl_format, youtube_dl_url, "-o", download_directory]
+        finish = subprocess.check_output(command_to_exec, stderr=subprocess.STDOUT)
+        time.sleep(5)
         client.send_chat_action(message.chat.id,'UPLOAD_AUDIO')
         sent = client.send_audio(message.chat.id, audio=download_directory, caption=description, title=ytitle, thumb=thumb_image_path, reply_to_message_id=msg_id).message_id
         t2 = time.time()
         client.edit_message_caption(message.chat.id,sent,caption='{}\nCompleted in {} Seconds'.format(description,str(int(t2-t1))))
         client.delete_messages(message.chat.id, sent_id)
-    except Exception as error:
-        client.edit_message_caption(message.chat.id, sent_id,'Could not send the mp3 file')
+    except subprocess.CalledProcessError as exc:
+        client.edit_message_caption(message.chat.id, sent_id,'Could not send the mp3 file with error: \n\n**{}**'.format(exc))
         print(error)
     client.send_chat_action(message.chat.id,'CANCEL')
     os.remove(title)
