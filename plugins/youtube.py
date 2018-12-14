ff7ba56e902c234ada4b045ac5a41e86b876240d
Ningmua Bruno <gwatanajohn3@gmail.com>
2018-12-14 06:53:51 +0100

    Update youtube.py

--- plugins/youtube.py
+++ plugins/youtube.py
@@ -53,18 +53,13 @@ def download(message, client, sent_id, text, msg_id,nome):
 	a = re[0]
 	print(a)
 	number = '-'+a.replace('[youtube] ','').replace(': Downloading webpage','')
-	client.edit_message_caption(message.chat.id, sent_id,'**Uploading your `{}` to telegram in progress**'.format(nome))
+	client.edit_message_caption(message.chat.id, sent_id,'Sending {}'.format(nome))
 	try:
-		time.sleep(5)
 		client.send_chat_action(message.chat.id,'UPLOAD_VIDEO')
 		sent = client.send_document(message.chat.id,title,caption=nome,reply_to_message_id=msg_id).message_id
 		t2 = time.time()
-		client.edit_message_caption(message.chat.id,sent,caption='{}\n**Completed in**:  `{}` **Seconds**'.format(nome,str(int(t2-t1))))
-		time.sleep(3)
-		description = " " + " \r\n© Made with ❤️ by @Bfas237Bots "
-		client.edit_message_caption(message.chat.id,sent,caption='\n{}\n'.format(description))
+		client.edit_message_caption(message.chat.id,sent,caption='{}\nCompleted in {} Seconds'.format(nome,str(int(t2-t1))))
 		client.delete_messages(message.chat.id, sent_id)
-		client.delete_messages(message.chat.id, msg_id)
 	except Exception as error:
 		client.edit_message_caption(message.chat.id, sent_id,'Could not send the video')
 		print(error)
@@ -78,38 +73,35 @@ def ytdlv(message,client):
 	if text == '':
 		client.send_message(
 			chat_id=chat_id,
-			text='**Usage:** `/ytdl video link or Video name`',
+			text='Uso: /ytdl URL do vídeo ou nome',
 			reply_to_message_id=msg_id
 		)
 	elif 'youtu.be' in text or 'youtube.com' in text:
 		sent_id = client.send_message(
 			chat_id=chat_id,
-			text='⏳ **Obtaining Video Information...**',
+			text='Obtaining Video Information...',
 			parse_mode='Markdown',
 			reply_to_message_id=msg_id
 		).message_id
 		a = search_query_yt(text)
-		time.sleep(5)
 		title = a['bot_api_yt'][0]['title']
 		thumb = a['bot_api_yt'][0]['url'].split('v=')[1]
 	else:
 		sent_id = client.send_message(
 			chat_id=chat_id,
-			text='🔍 **Searching the video on YouTube...**',
+			text='Searching the video on YouTube...',
 			parse_mode='Markdown',
 			reply_to_message_id=msg_id
 		).message_id
 		a = search_query_yt(text)
-		time.sleep(5)
 		text = a['bot_api_yt'][0]['url']
 		title = a['bot_api_yt'][0]['title']
 		thumb = text.split('v=')[1]
 	client.delete_messages(message.chat.id, sent_id)
 	print('https://i.ytimg.com/vi/{}/hqdefault.jpg'.format(thumb))
 	try:
-		sent_id = client.send_photo(message.chat.id,'https://i.ytimg.com/vi/{}/hqdefault.jpg'.format(thumb) ,caption='**Downloading:** {}'.format(title)).message_id
+		sent_id = client.send_photo(message.chat.id,'https://i.ytimg.com/vi/{}/hqdefault.jpg'.format(thumb) ,caption='Downloading: {}'.format(title)).message_id
 	except:
-		sent_id = client.send_photo(message.chat.id,'yt.png' ,caption='**Downloading:** {}'.format(title)).message_id
+		sent_id = client.send_photo(message.chat.id,'yt.png' ,caption='Downloading: {}'.format(title)).message_id
 	nome = title
-	time.sleep(5)
 	exec_thread(download,message,client,sent_id,text,msg_id,nome)
