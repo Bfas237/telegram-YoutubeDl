d1fd86268573e75c3ca12ae838a82fb9bf2d3242
Ningmua Bruno <gwatanajohn3@gmail.com>
2018-12-16 21:32:19 +0100

    Create check.py

---
+++ plugins/check.py
@@ -0,0 +1,30 @@
+from pyrogram import Client, Filters
+import config
+import time
+import urbandict as ud
+import sys, os, re, sys
+import warnings, random
+from random import randint
+import requests
+import threading
+import io
+
+@app.on_message(Filters.text & Filters.chat("Bfas237group"))
+def move(client, message):
+  if "Oft" in message.text:
+      client.send_message("bfas237off", "[{}](tg://user?id={}) **wrote:**\n{}\n\n**⬇️ ᴘʟᴇᴀsᴇ ᴄᴏɴᴛɪɴᴜᴇ ʜᴇʀᴇ ⬇️**".format(message.reply_to_message.from_user.first_name, message.reply_to_message.from_user.id, message.reply_to_message.text))
+      message.reply("I moved this discussion to the [Offtopic Group ↗️](https://t.me/bfas237off/{})".format(message.reply_to_message.message_id, message.reply_to_message.text), reply_to_message_id=message.reply_to_message.message_id, quote=True)
+      client.delete_messages(
+    "Bfas237group",
+    message_ids=message.message_id
+)
+    
+@app.on_message(Filters.text & Filters.chat("bfas237off"))
+def move(client, message):
+    if "Ont" in message.text:
+        client.send_message("Bfas237group", "[{}](tg://user?id={}) **wrote:**\n{}\n\n**⬇️ ᴘʟᴇᴀsᴇ ᴄᴏɴᴛɪɴᴜᴇ ʜᴇʀᴇ ⬇️**".format(message.reply_to_message.from_user.first_name, message.reply_to_message.from_user.id, message.reply_to_message.text))
+        message.reply("The Main group has been created for this discussion so why not join [Offtopic Group ↗️](https://t.me/bfas237off/{})".format(message.reply_to_message.message_id, message.reply_to_message.text), reply_to_message_id=message.reply_to_message.message_id, quote=True)
+        client.delete_messages(
+    "bfas237off",
+    message_ids=message.message_id
+)
