75db83a93202e6b8819b633cd236281eee9abde7
Ningmua Bruno <gwatanajohn3@gmail.com>
2018-12-17 17:11:43 +0100

    Update check.py

--- plugins/check.py
+++ plugins/check.py
@@ -1,20 +1,35 @@
-from pyrogram import Client, Filters
+# MIT License
+#
+# Copyright (c) 2018 Dan Tès <https://github.com/delivrance>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from pyrogram import Client, Emoji, Filters
 import config
-import time
-import urbandict as ud
-import sys, os, re, sys
-import warnings, random
-from random import randint
-import requests
-import threading
-import io
+MENTION = "[{}](tg://user?id={})"
+MESSAGE = "{} Welcome to [Bfas237 Group](https://bfas237blog.com/)'s group chat {}!"
 app = config.app
-user_id = config.user_id
-prefix = config.prefix
-
-
-@app.on_message(Filters.text & Filters.chat("Bfas237group"))
-def check(client, message):
+chats_filter = Filters.chat(["Bfas237Off", "Bfas237group"])
+off_topic = Filters.chat(["Bfas237Off"])
+on_topic = Filters.chat(["Bfas237group"])
+@app.on_message(on_topic & Filters.text)
+def ontopic(client, message):
   if "Oft" in message.text:
       client.send_message("bfas237off", "[{}](tg://user?id={}) **wrote:**\n{}\n\n**⬇️ ᴘʟᴇᴀsᴇ ᴄᴏɴᴛɪɴᴜᴇ ʜᴇʀᴇ ⬇️**".format(message.reply_to_message.from_user.first_name, message.reply_to_message.from_user.id, message.reply_to_message.text))
       message.reply("I moved this discussion to the [Offtopic Group ↗️](https://t.me/bfas237off/{})".format(message.reply_to_message.message_id, message.reply_to_message.text), reply_to_message_id=message.reply_to_message.message_id, quote=True)
@@ -22,9 +37,11 @@ def check(client, message):
     "Bfas237group",
     message_ids=message.message_id
 )
-    
-@app.on_message(Filters.text & Filters.chat("bfas237off"))
-def checkd(client, message):
+
+      
+      
+@app.on_message(off_topic & Filters.text)
+def offtopic(client, message):
     if "Ont" in message.text:
         client.send_message("Bfas237group", "[{}](tg://user?id={}) **wrote:**\n{}\n\n**⬇️ ᴘʟᴇᴀsᴇ ᴄᴏɴᴛɪɴᴜᴇ ʜᴇʀᴇ ⬇️**".format(message.reply_to_message.from_user.first_name, message.reply_to_message.from_user.id, message.reply_to_message.text))
         message.reply("The Main group has been created for this discussion so why not join [Offtopic Group ↗️](https://t.me/bfas237off/{})".format(message.reply_to_message.message_id, message.reply_to_message.text), reply_to_message_id=message.reply_to_message.message_id, quote=True)
