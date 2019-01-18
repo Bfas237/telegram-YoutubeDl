65b339fb921005b2e159dcfc4e32d15e9c6a208b
Ningmua Bruno <gwatanajohn3@gmail.com>
2019-01-18 23:47:11 +0100

    Update haste.py

--- plugins/haste.py
+++ plugins/haste.py
@@ -1,37 +1,21 @@
-# MIT License
-#
-# Copyright (c) 2018 Dan Tès <https://github.com/delivrance>
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
 import requests
 
 from pyrogram import Client, Filters
-
-BASE = "https://hastebin.com"
 import config
-
+try:
+    import aiohttp
+except ImportError:
+    aiohttp = None
+    logging.warning('aiohttp module not available; #haste command disabled')
 app = config.app
 user_id = config.user_id
 
+BASE = "https://hastebin.com"
+def post(content):
+    post = requests.post("https://hastebin.com/documents", data=content.encode('utf-8'))
+    return "https://hastebin.com/" + post.json()["key"]
 
-@app.on_message(Filters.command("haste", prefix="!") & Filters.reply)
+@Client.on_message(Filters.command("hb", prefix=".") & Filters.reply)
 def haste(client, message):
     reply = message.reply_to_message
 
@@ -39,13 +23,13 @@ def haste(client, message):
         return
 
     message.delete()
-
-    result = requests.post(
-        "{}/documents".format(BASE),
-        data=reply.text.encode("UTF-8")
-    ).json()
-
-    message.reply(
-        "{}/{}.py".format(BASE, result["key"]),
+    with aiohttp.ClientSession() as session:
+       with post(reply.text) as resp:
+        if resp.status >= 300:
+            message.edit("Hastebin seems to be down… ( ^^')")
+             return
+        haste = (resp.json())['key']
+    message.edit(
+        "**Complete!:** Check it at {}/{}.py".format(BASE, haste["key"]),
         reply_to_message_id=reply.message_id
     )
