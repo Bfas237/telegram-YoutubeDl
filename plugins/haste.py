d9045ae15ec50adb3a4191c417d0786eb396e28d
Ningmua Bruno <gwatanajohn3@gmail.com>
2019-01-19 00:37:07 +0100

    Update haste.py

--- plugins/haste.py
+++ plugins/haste.py
@@ -1,39 +1,47 @@
+from pyrogram import Filters
+import config
 import requests
 
-from pyrogram import Client, Filters
-import config
-try:
-    import aiohttp
-except ImportError:
-    aiohttp = None
-    logging.warning('aiohttp module not available; #haste command disabled')
 app = config.app
 user_id = config.user_id
 
-BASE = "https://hastebin.com"
-def post(content):
-    post = requests.post("https://hastebin.com/documents", data=content.encode('utf-8'))
-    return "https://hastebin.com/" + post.json()["key"]
 
-@Client.on_message(Filters.command("hb", prefix=".") & Filters.reply)
-def haste(client, message):
-    reply = message.reply_to_message
+if config.language == "english":
+    from languages.english import eval_running_text, eval_error_text, eval_success_text, eval_result_text
 
-    if reply.text is None:
-        client.edit_message_text(
-                message.chat.id,
-                message.message_id,
-                "You didn't use me correctly so try again"
+RUNNING = "**Uploading to:** a paste center for you"
+ERROR = "**There was a glitch:**\n```{}```\n**Error:**\n```{}```"
+NoSUCCESS = "**Sorry i had to end unexpectedly:**\n```{}```\n**None**"
+RESULT = "**Result:**\n{}"
+https://hastebin.com/
+
+@app.on_message(Filters.command("paste", prefix="!"))
+def paste(client, message):
+    haste = " ".join(message.command[1:])
+
+    if haste:
+        if message.reply_to_message:
+            m = message.reply(RUNNING.format(haste))
+
+        try:
+            result = requests.post("https://hastebin.com/documents", data=content.encode('utf-8'))
+        except Exception as error:
+            client.edit_message_text(
+                m.chat.id,
+                m.message_id,
+                ERROR.format(haste, error)
             )
-        return
-
-    message.delete()
-    with aiohttp.ClientSession() as session:
-       with post(reply.text) as resp:
-        if resp.status >= 300:
-            message.edit("Hastebin seems to be down… ( ^^')")
-             return
-    message.edit(
-        "**Complete!:** Check it at {}/{}.py".format(BASE, resp),
-        reply_to_message_id=reply.message_id
-    )
+        else:
+            if not result:
+                client.edit_message_text(
+                    m.chat.id,
+                    m.message_id,
+                    NoSUCCESS.format(haste)
+                )
+            else:
+                go = "https://hastebin.com/" + post.json()["key"]
+                client.edit_message_text(
+                    m.chat.id,
+                    m.message_id,
+                    RESULT.format(go)
+                )
