5432fe67ff3add7f13044189da97cea0c6e62157
Ningmua Bruno <gwatanajohn3@gmail.com>
2019-01-20 12:01:12 +0100

    Update haste.py

--- plugins/haste.py
+++ plugins/haste.py
@@ -1,18 +1,29 @@
 from pyrogram import Filters
 import config
 import requests
-
+BASE = "https://hastebin.com"
 app = config.app
 user_id = config.user_id
 
 
 from pyrogram import Client, Filters
     
-@app.on_message(Filters.command("r", prefix="!") & Filters.reply & ~Filters.edited & Filters.group)
-def r(client, message):
-    if len(message.command) > 1:
-        colength = len("r") + len("!")
-        query = str(message.text)[colength:].lstrip()
-        eventsplit=query.split("/")
-        result="**You mean:**\n{}".format(message.reply_to_message.text.replace(eventsplit[0],eventsplit[1]))
-        client.edit_message_text(message.chat.id, message.message_id, result)
+@app.on_message(Filters.command("hb", prefix="!") & Filters.reply)
+def eval_expression(client, message):
+    expression = " ".join(message.command[1:])
+    reply = message.reply_to_message
+
+    if reply.text is None:
+        return
+
+    message.delete()
+
+    result = requests.post(
+        "{}/documents".format(BASE),
+        data=reply.text.encode("UTF-8")
+    ).json()
+
+    message.reply(
+        "{}/{}.py".format(BASE, result["key"]),
+        reply_to_message_id=reply.message_id
+    )
