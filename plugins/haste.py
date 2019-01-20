f90a1f3dfd5d5b286db21c45de40c46ee2387b73
Ningmua Bruno <gwatanajohn3@gmail.com>
2019-01-20 11:51:03 +0100

    Update haste.py

--- plugins/haste.py
+++ plugins/haste.py
@@ -6,42 +6,13 @@ app = config.app
 user_id = config.user_id
 
 
-if config.language == "english":
-    from languages.english import eval_running_text, eval_error_text, eval_success_text, eval_result_text
-
-RUNNING = "**Uploading to:** a paste center for you"
-ERROR = "**There was a glitch:**\n```{}```\n**Error:**\n```{}```"
-NoSUCCESS = "**Sorry i had to end unexpectedly:**\n```{}```\n**None**"
-RESULT = "**Result:**\n{}"
-https://hastebin.com/
-
-@app.on_message(Filters.command("paste", prefix="!"))
-def paste(client, message):
-    haste = " ".join(message.command[1:])
-
-    if haste:
-        if message.reply_to_message:
-            m = message.reply(RUNNING.format(haste))
-
-        try:
-            result = requests.post("https://hastebin.com/documents", data=content.encode('utf-8'))
-        except Exception as error:
-            client.edit_message_text(
-                m.chat.id,
-                m.message_id,
-                ERROR.format(haste, error)
-            )
-        else:
-            if not result:
-                client.edit_message_text(
-                    m.chat.id,
-                    m.message_id,
-                    NoSUCCESS.format(haste)
-                )
-            else:
-                go = "https://hastebin.com/" + post.json()["key"]
-                client.edit_message_text(
-                    m.chat.id,
-                    m.message_id,
-                    RESULT.format(go)
-                )
+from pyrogram import Client, Filters
+    
+@Client.on_message(Filters.command("r", prefix="!") & Filters.reply & ~Filters.edited & Filters.group)
+def r(client, message):
+    if len(message.command) > 1:
+        colength = len("r") + len("!")
+        query = str(message.text)[colength:].lstrip()
+        eventsplit=query.split("/")
+        result="**You mean:**\n{}".format(message.reply_to_message.text.replace(eventsplit[0],eventsplit[1]))
+        client.edit_message_text(message.chat.id, message.message_id, result)
