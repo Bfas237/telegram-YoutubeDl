62b6e8984de2a425e569ff9b3556b095d92fca2a
Ningmua Bruno <gwatanajohn3@gmail.com>
2019-02-28 17:26:54 +0100

    Update youtube.py

--- plugins/youtube.py
+++ plugins/youtube.py
@@ -18,9 +18,9 @@ import pyrogram
 ydl = youtube_dl.YoutubeDL({'outtmpl': 'dls/%(title)s.%(ext)s', 'format': '140', 'noplaylist': True})
 
 def exec_thread(target, *args, **kwargs):
-	t = threading.Thread(target=target, args=args, kwargs=kwargs)
-	t.daemon = True
-	t.start()
+    t = threading.Thread(target=target, args=args, kwargs=kwargs)
+    t.daemon = True
+    t.start()
 
 def pretty_size(size):
     units = ['B', 'KB', 'MB', 'GB']
@@ -51,38 +51,38 @@ def search_ytdd(query):
     return list_videos
 
 def download(message, client, sent_id, text, msg_id,nome):
-	t1 = time.time()
-	res = subprocess.getstatusoutput("""cd dls 
+    t1 = time.time()
+    res = subprocess.getstatusoutput("""cd dls 
 youtube-dl '{}'""".format(text))[1]
-	re = []
-	for	i in res.split('\n'):
-		re.append(i)
-	for i in re:
-		if '[download] ' in i:
-			if '.mp4' in i or '.webm' in i:
-				title = i
-	title = title.replace('[download] ','')
-	if ' has already been downloaded' in title:
-		title = title.replace(' has already been downloaded','')
-	if 'Destination: ' in title:
-		title = title.replace('Destination: ','')
-	print(title)
-	a = re[0]
-	print(a)
-	number = '-'+a.replace('[youtube] ','').replace(': Downloading webpage','')
-	client.edit_message_caption(message.chat.id, sent_id,'Enviando {}'.format(nome))
-	try:
-		client.send_chat_action(message.chat.id,'UPLOAD_VIDEO')
-		sent = client.send_document(message.chat.id,'dls/'+title,caption=nome,reply_to_message_id=msg_id).message_id
-		t2 = time.time()
-		client.edit_message_caption(message.chat.id,sent,caption='{}\nDemorou {} segundos\n© Made with ❤️ by @Bfas237Bots'.format(nome,str(int(t2-t1))))
-		client.delete_messages(message.chat.id, sent_id)
-	except Exception as error:
-		client.edit_message_caption(message.chat.id, sent_id,'não foi possiviel enviar')
-		print(error)
-	client.send_chat_action(message.chat.id,'CANCEL')
-	os.remove('dls/'+title)
-	
+    re = []
+    for i in res.split('\n'):
+        re.append(i)
+    for i in re:
+        if '[download] ' in i:
+            if '.mp4' in i or '.webm' in i:
+                title = i
+    title = title.replace('[download] ','')
+    if ' has already been downloaded' in title:
+        title = title.replace(' has already been downloaded','')
+    if 'Destination: ' in title:
+        title = title.replace('Destination: ','')
+    print(title)
+    a = re[0]
+    print(a)
+    number = '-'+a.replace('[youtube] ','').replace(': Downloading webpage','')
+    client.edit_message_caption(message.chat.id, sent_id,'Enviando {}'.format(nome))
+    try:
+        client.send_chat_action(message.chat.id,'UPLOAD_VIDEO')
+        sent = client.send_document(message.chat.id,'dls/'+title,caption=nome,reply_to_message_id=msg_id).message_id
+        t2 = time.time()
+        client.edit_message_caption(message.chat.id,sent,caption='{}\nDemorou {} segundos\n© Made with ❤️ by @Bfas237Bots'.format(nome,str(int(t2-t1))))
+        client.delete_messages(message.chat.id, sent_id)
+    except Exception as error:
+        client.edit_message_caption(message.chat.id, sent_id,'não foi possiviel enviar')
+        print(error)
+    client.send_chat_action(message.chat.id,'CANCEL')
+    os.remove('dls/'+title)
+    
 def cytdlv(message,client):
     text = message.text[4:]
     chat_id = message.chat.id
@@ -128,7 +128,8 @@ def cytdlv(message,client):
     time.sleep(2)
     exec_thread(download,message,client,sent_id,text,msg_id,nome)
 
-
+from pytube import YouTube 
+SAVE_PATH = "tmp/"
 
 def search_query_yt(query):
     url_base = "https://www.youtube.com/results"
@@ -152,70 +153,64 @@ def search_query_yt(query):
     return dic
 
 def ytdlv(message,client):
-	text = message.text[5:]
-	chat_id = message.chat.id
-	msg_id = message.message_id
-	if text == '':
-		client.send_message(
-			chat_id=chat_id,
-			text='Uso: /vid URL do vídeo ou nome',
-			reply_to_message_id=msg_id
-		)
-	elif 'youtu.be' in text or 'youtube.com' in text:
-		sent_id = client.send_message(
-			chat_id=chat_id,
-			text='Obtendo informações do vídeo...',
-			parse_mode='Markdown',
-			reply_to_message_id=msg_id
-		).message_id
-		a = search_query_yt(text)
-		title = a['bot_api_yt'][0]['title']
-		thumb = a['bot_api_yt'][0]['url'].split('v=')[1]
-	else:
-		sent_id = client.send_message(
-			chat_id=chat_id,
-			text='Pesquisando o vídeo no YouTube...',
-			parse_mode='Markdown',
-			reply_to_message_id=msg_id
-		).message_id
-		a = search_query_yt(text)
-		text = a['bot_api_yt'][0]['url']
-		title = a['bot_api_yt'][0]['title']
-		thumb = text.split('v=')[1]
-	client.delete_messages(message.chat.id, sent_id)
-	print('https://i.ytimg.com/vi/{}/hqdefault.jpg'.format(thumb))
-	try:
-		sent_id = client.send_photo(message.chat.id,'https://i.ytimg.com/vi/{}/hqdefault.jpg'.format(thumb) ,caption='baixando: {}'.format(title)).message_id
-	except:
-		sent_id = client.send_photo(message.chat.id,'yt.png' ,caption='baixando: {}'.format(title)).message_id
-	nome = title
-	from pytube import YouTube 
-	SAVE_PATH = "tmp/"
-	try: 
-		yt = YouTube(text) 
-	except Exception as e:
-        	client.send_photo(message.chat.id,'yt.png' ,caption='An error Occured\n\n' + str(e))
-		print("Connection Error") 
-		yt.streams.filter(progressive=True).all()
-		yt.register_on_progress_callback(show_progress_bar)
-		fn = yt.streams.first()
-		try: 
-			print(fn)
-			fn.download(SAVE_PATH)
-		except Exception as e:
-        		client.send_photo(message.chat.id,'yt.png' ,caption='An error Occuredsss0\n\n' + str(e))
+    text = message.text[5:]
+    chat_id = message.chat.id
+    msg_id = message.message_id
+    if text == '':
+        client.send_message(
+            chat_id=chat_id,
+            text='Uso: /vid URL do vídeo ou nome',
+            reply_to_message_id=msg_id
+        )
+    elif 'youtu.be' in text or 'youtube.com' in text:
+        sent_id = client.send_message(
+            chat_id=chat_id,
+            text='Obtendo informações do vídeo...',
+            parse_mode='Markdown',
+            reply_to_message_id=msg_id
+        ).message_id
+        a = search_query_yt(text)
+        title = a['bot_api_yt'][0]['title']
+        thumb = a['bot_api_yt'][0]['url'].split('v=')[1]
+    else:
+        sent_id = client.send_message(
+            chat_id=chat_id,
+            text='Pesquisando o vídeo no YouTube...',
+            parse_mode='Markdown',
+            reply_to_message_id=msg_id
+        ).message_id
+        a = search_query_yt(text)
+        text = a['bot_api_yt'][0]['url']
+        title = a['bot_api_yt'][0]['title']
+        thumb = text.split('v=')[1]
+    client.delete_messages(message.chat.id, sent_id)
+    print('https://i.ytimg.com/vi/{}/hqdefault.jpg'.format(thumb))
+    try:
+        sent_id = client.send_photo(message.chat.id,'https://i.ytimg.com/vi/{}/hqdefault.jpg'.format(thumb) ,caption='baixando: {}'.format(title)).message_id
+    except:
+        sent_id = client.send_photo(message.chat.id,'yt.png' ,caption='baixando: {}'.format(title)).message_id
+        nome = title
+    
+    try: 
+        yt = YouTube(text) 
+    except Exception as e:
+        client.send_photo(message.chat.id,'yt.png' ,caption='An error Occured\n\n' + str(e))
+        print("Connection Error") 
+        yt.streams.filter(progressive=True).all()
+        yt.register_on_progress_callback(show_progress_bar)
+        fn = yt.streams.first()
+        try: 
+            print(fn)
+            fn.download(SAVE_PATH)
+            client.edit_message_caption(message.chat.id, sent_id,'Enviando {}'.format(title))
 
-	client.edit_message_caption(message.chat.id, sent_id,'Enviando {}'.format(title))
-	try:
-		client.send_chat_action(message.chat.id,'UPLOAD_VIDEO')
-		sent = client.send_document(message.chat.id, SAVE_PATH, caption=title,reply_to_message_id=msg_id).message_id
-		t2 = time.time()
-		client.edit_message_caption(message.chat.id,sent,caption='{}\n\n© Made with ❤️ by @Bfas237Bots'.format(title))
-		client.delete_messages(message.chat.id, sent_id)
-	except Exception as error:
-		client.edit_message_caption(message.chat.id, sent_id,'não foi possiviel enviar')
-		print(error)
-		client.send_chat_action(message.chat.id,'CANCEL')
-		
-		
-		
+            client.send_chat_action(message.chat.id,'UPLOAD_VIDEO')
+            sent = client.send_document(message.chat.id, SAVE_PATH, caption=title,reply_to_message_id=msg_id).message_id
+            t2 = time.time()
+            client.edit_message_caption(message.chat.id,sent,caption='{}\n\n© Made with ❤️ by @Bfas237Bots'.format(title))
+            client.delete_messages(message.chat.id, sent_id)
+        except Exception as e:
+            client.send_photo(message.chat.id,'yt.png' ,caption='An error Occuredsss0\n\n' + str(e))
+            print(str(e))
+            client.send_chat_action(message.chat.id,'CANCEL')
+
