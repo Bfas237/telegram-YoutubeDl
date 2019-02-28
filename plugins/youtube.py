7e780a3b4819a2154ada3a167dfcd1135524b06a
Ningmua Bruno <gwatanajohn3@gmail.com>
2019-02-28 20:02:58 +0100

    Update youtube.py

--- plugins/youtube.py
+++ plugins/youtube.py
@@ -15,6 +15,10 @@ import youtube_dl
 import unidecode as ud
 import pyrogram
 
+from hachoir.metadata import extractMetadata
+from hachoir.parser import createParser
+# https://stackoverflow.com/a/37631799/4723940
+from PIL import Image
 ydl = youtube_dl.YoutubeDL({'outtmpl': 'dls/%(title)s.%(ext)s', 'format': '140', 'noplaylist': True})
 
 def exec_thread(target, *args, **kwargs):
@@ -50,84 +54,71 @@ def search_ytdd(query):
             pass
     return list_videos
 
-def download(message, client, sent_id, text, msg_id,nome):
-    t1 = time.time()
-    res = subprocess.getstatusoutput("""cd dls 
-youtube-dl '{}'""".format(text))[1]
-    re = []
-    for i in res.split('\n'):
-        re.append(i)
-    for i in re:
-        if '[download] ' in i:
-            if '.mp4' in i or '.webm' in i:
-                title = i
-    title = title.replace('[download] ','')
-    if ' has already been downloaded' in title:
-        title = title.replace(' has already been downloaded','')
-    if 'Destination: ' in title:
-        title = title.replace('Destination: ','')
-    print(title)
-    a = re[0]
-    print(a)
-    number = '-'+a.replace('[youtube] ','').replace(': Downloading webpage','')
-    client.edit_message_caption(message.chat.id, sent_id,'Enviando {}'.format(nome))
-    try:
-        client.send_chat_action(message.chat.id,'UPLOAD_VIDEO')
-        sent = client.send_document(message.chat.id,'dls/'+title,caption=nome,reply_to_message_id=msg_id).message_id
-        t2 = time.time()
-        client.edit_message_caption(message.chat.id,sent,caption='{}\nDemorou {} segundos\n© Made with ❤️ by @Bfas237Bots'.format(nome,str(int(t2-t1))))
-        client.delete_messages(message.chat.id, sent_id)
-    except Exception as error:
-        client.edit_message_caption(message.chat.id, sent_id,'não foi possiviel enviar')
-        print(error)
-    client.send_chat_action(message.chat.id,'CANCEL')
-    os.remove('dls/'+title)
-    
-def cytdlv(message,client):
-    text = message.text[4:]
-    chat_id = message.chat.id
-    msg_id = message.message_id
-    if text == '':
-        client.send_message(
-            chat_id=chat_id,
-            text='**Usage:** `!vid vídeo URL or name`',
-            reply_to_message_id=msg_id
-        )
-    elif 'youtu.be' in text or 'youtube.com' in text:
-        sent_id = client.send_message(
-            chat_id=chat_id,
-            text='⏳ **Obtaining Video Information From Youtube...**',
-            parse_mode='Markdown',
-            reply_to_message_id=msg_id
-        ).message_id
-        a = ydl.extract_info('ytsearch:' + text, download=False)['entries'][0]
-        time.sleep(3)
-    else:
-        sent_id = client.send_message(
-            chat_id=chat_id,
-            text='🔍 **Searching the video on Online...**',
-            parse_mode='Markdown',
-            reply_to_message_id=msg_id
-        ).message_id
-        a = ydl.extract_info(text, download=False)
-        time.sleep(3)
-    for f in a['formats']:
-        if f['format_id'] == '140':
-            fsize = f['filesize']
-            name = a['title']
-            text = a['url']
-            title = a['title']
-            thumb = text.split('v=')
-        client.delete_messages(message.chat.id, sent_id)
-        print('https://i.ytimg.com/vi/{}/hqdefault.jpg'.format(thumb))
-    try:
-        sent_id = client.send_photo(message.chat.id,'https://i.ytimg.com/vi/{}/hqdefault.jpg'.format(thumb) ,caption='**Downloading:** `{}`'.format(title)).message_id
-    except Exception as e:
-        sent_id = client.send_photo(message.chat.id,'yt.png' ,caption='An error Occured\n\n' + str(e)).message_id
-    nome = title
-    time.sleep(2)
-    exec_thread(download,message,client,sent_id,text,msg_id,nome)
+def DetectFileSize(url):
+    r = requests.get(url, allow_redirects=True, stream=True)
+    total_size = int(r.headers.get("content-length", 0))
+    return total_size
 
+
+
+def progress_for_pyrogram(client, current, total, ud_type, message_id, chat_id):
+    """if round(current / total * 100, 0) % 10 == 0:
+        try:
+            client.edit_message_text(
+                chat_id,
+                message_id,
+                text="{}: {} of {}".format(
+                    ud_type,
+                    humanbytes(current),
+                    humanbytes(total)
+                )
+            )
+        except:
+            pass"""
+    # logger.info("{}: {} of {}".format(ud_type, humanbytes(current), humanbytes(total)))
+    pass
+
+
+def humanbytes(size):
+    # https://stackoverflow.com/a/49361727/4723940
+    # 2**10 = 1024
+    if not size:
+        return ""
+    power = 2**10
+    n = 0
+    Dic_powerN = {0: ' ', 1: 'Ki', 2: 'Mi', 3: 'Gi', 4: 'Ti'}
+    while size > power:
+        size /= power
+        n += 1
+    return str(math.floor(size)) + " " + Dic_powerN[n] + 'B'
+def DownLoadFile(url, file_name, chunk_size, client, ud_type, message_id, chat_id):
+    if os.path.exists(file_name):
+        os.remove(file_name)
+    r = requests.get(url, allow_redirects=True, stream=True)
+    # https://stackoverflow.com/a/47342052/4723940
+    total_size = int(r.headers.get("content-length", 0))
+    downloaded_size = 0
+    with open(file_name, 'wb') as fd:
+        for chunk in r.iter_content(chunk_size=chunk_size):
+            if chunk:
+                fd.write(chunk)
+                downloaded_size += chunk_size
+            if client is not None:
+                if ((total_size // downloaded_size) % 5) == 0:
+                    time.sleep(0.3)
+                    try:
+                        client.edit_message_text(
+                            chat_id,
+                            message_id,
+                            text="{}: {} of {}".format(
+                                ud_type,
+                                humanbytes(downloaded_size),
+                                humanbytes(total_size)
+                            )
+                        )
+                    except:
+                        pass
+    return file_name
 from pytube import YouTube 
 SAVE_PATH = "tmp/"
 
@@ -194,9 +185,45 @@ def ytdlv(message,client):
         title = a['bot_api_yt'][0]['title']
         thumb = text.split('v=')[1]
         client.delete_messages(message.chat.id, sent_id)
-        thumb_image_path = 'https://i.ytimg.com/vi/{}/hqdefault.jpg'.format(thumb)
-        print('https://i.ytimg.com/vi/{}/hqdefault.jpg'.format(thumb))
+        width = 0
+        height = 0
+        duration = 0
+        thumbnail_image = SAVE_PATH + 'https://i.ytimg.com/vi/{}/hqdefault.jpg'.format(thumb)
         try:
+            thumb_image_path = DownLoadFile(
+                thumbnail_image,
+                SAVE_PATH + str(update.from_user.id) + ".jpg",
+                8192,
+                None,  # bot,
+                "Downloading:",
+                sent_id,
+                message.chat.id
+            )
+
+            print(thumbnail_image)
+            metadata = extractMetadata(createParser(download_directory))
+            if metadata.has("duration"):
+                duration = metadata.get('duration').seconds
+            if os.path.exists(thumb_image_path):
+                width = 0
+                height = 0
+                metadata = extractMetadata(createParser(thumb_image_path))
+                if metadata.has("width"):
+                    width = metadata.get("width")
+                if metadata.has("height"):
+                    height = metadata.get("height")
+                # resize image
+                # ref: https://t.me/PyrogramChat/44663
+                # https://stackoverflow.com/a/21669827/4723940
+                Image.open(thumb_image_path).convert(
+                    "RGB").save(thumb_image_path)
+                img = Image.open(thumb_image_path)
+                # https://stackoverflow.com/a/37631799/4723940
+                img.thumbnail((90, 90))
+                img.save(thumb_image_path, "JPEG")
+                # https://pillow.readthedocs.io/en/3.1.x/reference/Image.html#create-thumbnails
+            else:
+                thumb_image_path = None
             sent_id = client.send_photo(message.chat.id,'https://i.ytimg.com/vi/{}/hqdefault.jpg'.format(thumb) ,caption='Downloading: {}'.format(title)).message_id
             yt = YouTube(text).streams.filter(subtype='mp4', progressive=True).first()
             file_name = ud.unidecode(YouTube(text).title.replace(" ", "_").replace(".", "-"))
@@ -206,7 +233,7 @@ def ytdlv(message,client):
             client.edit_message_caption(message.chat.id, sent_id,'Uploading... {}'.format(title))
             final = SAVE_PATH + '{}.mp4'.format(file_name)
             client.send_chat_action(message.chat.id,'UPLOAD_VIDEO')
-            sent = client.send_video(message.chat.id, video=final, caption=title,supports_streaming=True,thumb=thumb_image_path,reply_to_message_id=msg_id, progress = prog, progress_args = (sent_id, message.chat.id, final)).message_id
+            sent = client.send_video(message.chat.id, video=final, caption=title,duration=duration,width=width,height=height,supports_streaming=True,thumb=thumb_image_path,reply_to_message_id=msg_id, progress = prog, progress_args = (sent_id, message.chat.id, final)).message_id
             t2 = time.time()
             client.edit_message_caption(message.chat.id,sent,caption='{}\n\n© Made with ❤️ by @Bfas237Bots'.format(title))
             client.delete_messages(message.chat.id, sent_id)
